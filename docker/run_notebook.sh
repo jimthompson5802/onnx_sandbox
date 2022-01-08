b6987cf62d5ca1efacb7dc4b98aebcf014fc62ea
Jim Thompson <jimthompson5802@gmail.com>
2022-01-08 14:13:16 -0500

    add path to support config file specification

--- docker/run_notebook.sh
+++ docker/run_notebook.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# run jupyerlab notebook for
+# run jupyerlab notebook for analysis work
+# note: 2nd -v required for consistency of directory location for models and data
 docker run -it --rm \
   -p 8888:8888 \
   -v $PWD:/home/jovyan/project \
+  -v $PWD:/Users/jim/Desktop/onnx_sandbox \
   dsimages/onnx_testbed:v1 \
   jupyter lab --LabApp.token=''
