27f8d51767bb5c7ec6355a1188c7e14688c79929
Jim Thompson <jimthompson5802@gmail.com>
2022-01-08 09:49:41 -0500

    start common utils package

---
+++ utils/utils.py
@@ -0,0 +1,14 @@
+import joblib
+import os
+import tempfile
+
+import numpy as np
+
+# helper function to get approximate im-memory size of RF model in MB
+# https://mljar.com/blog/random-forest-memory/
+def rf_model_size_mb(model):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        rf_file = os.path.join(tmpdir, "rf_model")
+        joblib.dump(model, rf_file, compress=0)
+        rf_size = np.round(os.path.getsize(rf_file) / 1024 / 1024, 4)
+        return rf_size
