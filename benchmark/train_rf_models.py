b5df08a18d1ce136550b59c22cfc540c5ec87690
Jim Thompson <jimthompson5802@gmail.com>
2022-01-09 17:29:15 -0500

    add hyper-parameter for RF training, refactor output directory spec

--- benchmark/train_rf_models.py
+++ benchmark/train_rf_models.py
@@ -22,52 +22,55 @@ from skl2onnx.common.data_types import FloatTensorType
 with open('./config.yaml', 'r') as f:
     config = yaml.safe_load(f)
 
-DATA_DIR = config['data_dir']
-MODELS_DIR = config['models_dir']
+DATA_DIR = os.path.join(config['data_dir'], 'benchmark')
+MODELS_DIR = os.path.join(config['models_dir'], 'benchmark')
 
 # set up models directory
-models_dir = os.path.join(MODELS_DIR, 'benchmark')
-shutil.rmtree(models_dir, ignore_errors=True)
-os.makedirs(models_dir, exist_ok=True)
+shutil.rmtree(MODELS_DIR, ignore_errors=True)
+os.makedirs(MODELS_DIR, exist_ok=True)
 
 # train one county model and save as pickle and onnx files
 def train_a_model(county_id: str) -> Dict:
     print(f'Training county: {county_id}')
     try:
-        train_df = pd.read_parquet(os.path.join(DATA_DIR, 'benchmark', 'train.parquet'))
+        train_df = pd.read_parquet(os.path.join(DATA_DIR, 'train.parquet'))
         train_df = train_df.loc[train_df['county'] == county_id]
 
         X = train_df.drop(['county', 'y'], axis='columns')
         y = train_df['y']
 
-        rf = RandomForestRegressor(n_jobs=4, random_state=config['random_seed'])
+        rf = RandomForestRegressor(
+            n_estimators=config['number_trees'],
+            n_jobs=4,
+            random_state=config['random_seed']
+        )
         rf.fit(X, y)
         # print(f'RF fit() complete for {county_id}')
 
         # save model as pickle file
-        with open(os.path.join(models_dir, county_id+'.pkl'), 'wb') as f:
+        with open(os.path.join(MODELS_DIR, county_id+'.pkl'), 'wb') as f:
             pickle.dump(rf, f)
         # print(f"complete rf sklearn save for {county_id}")
 
         # save model as onnx
         explanatory_var = [('float_input', FloatTensorType([None, 20]))]
         onnx_model = convert_sklearn(rf, initial_types=explanatory_var)
-        with open(os.path.join(models_dir, county_id+'.onnx'), 'wb') as f:
+        with open(os.path.join(MODELS_DIR, county_id+'.onnx'), 'wb') as f:
             f.write(onnx_model.SerializeToString())
         # print(f'complete rf onnx save for {county_id}')
 
         print(f"completed training for {county_id}")
 
         return {county_id: "OK"}
-    except:
-        return {county_id: 'FAILED'}
+    except Exception as e:
+        return {county_id: f'FAILED: {e}'}
 
 
 if __name__ == '__main__':
     freeze_support()
 
     # get list of counties from training data set
-    df = pd.read_parquet(os.path.join(DATA_DIR, 'benchmark', 'train.parquet'))
+    df = pd.read_parquet(os.path.join(DATA_DIR, 'train.parquet'))
     county_list = df['county'].unique().tolist()
     del df
 
