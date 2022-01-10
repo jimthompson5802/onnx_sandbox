d2fe78756f1b900d6c0ca856f7bbc7d465064f4e
Jim Thompson <jimthompson5802@gmail.com>
2022-01-09 21:08:07 -0500

    another memory size test

--- utils/utils.py
+++ utils/utils.py
@@ -8,6 +8,9 @@ import sys
 import gc
 import numpy as np
 import pickle
+from collections import Mapping, Container
+
+
 
 from sklearn.ensemble import RandomForestRegressor
 from onnxruntime import InferenceSession
@@ -47,6 +50,44 @@ def actualsize(input_obj):
 
 def actualsize_mb(input_obj):
     return np.round(actualsize(input_obj) / (1024 * 1024), 4)
+
+
+# Yet Another Memory Reporter
+# https://code.tutsplus.com/tutorials/understand-how-much-memory-your-python-objects-use--cms-25609
+# DOES REPORT CORRECTLY RF object
+def deep_getsizeof(o, ids):
+    """Find the memory footprint of a Python object
+
+    This is a recursive function that drills down a Python object graph
+    like a dictionary holding nested dictionaries with lists of lists
+    and tuples and sets.
+
+    The sys.getsizeof function does a shallow size of only. It counts each
+    object inside a container as pointer only regardless of how big it
+    really is.
+
+    :param o: the object
+    :param ids:
+    :return:
+    """
+    raise DeprecationWarning('DEPRECATED')
+    d = deep_getsizeof
+    if id(o) in ids:
+        return 0
+
+    r = sys.getsizeof(o)
+    ids.add(id(o))
+
+    if isinstance(o, str) or isinstance(0, unicode):
+        return r
+
+    if isinstance(o, Mapping):
+        return r + sum(d(k, ids) + d(v, ids) for k, v in o.iteritems())
+
+    if isinstance(o, Container):
+        return r + sum(d(x, ids) for x in o)
+
+    return r
 
 # get file size in MB
 def get_file_size_mb(fp: str) -> np.float:
