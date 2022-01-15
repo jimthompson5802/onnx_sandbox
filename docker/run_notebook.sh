ae2596c368272eff55c5808134c4d26fabee4c7d
Jim Thompson <jimthompson5802@gmail.com>
2022-01-14 20:18:49 -0500

    docker image for only onnx scoring

--- docker/run_notebook.sh
+++ docker/run_notebook.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+docker_repository=${2:-onnx_testbed}
+tag=${1:-v1}
+
 # run jupyerlab notebook for analysis work
 # note: 2nd -v required for consistency of directory location for models and data
 docker run -it --rm \
   -p 8888:8888 \
   -v $PWD:/home/jovyan/project \
   -v $PWD:/Users/jim/Desktop/onnx_sandbox \
-  dsimages/onnx_testbed:v1 \
+  dsimages/${docker_repository}:${tag} \
   jupyter lab --LabApp.token=''
