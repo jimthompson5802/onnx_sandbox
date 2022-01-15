b2066546eebd3d073201bdacd1df621ae1c4c814
Jim Thompson <jimthompson5802@gmail.com>
2022-01-15 14:54:41 -0500

    shell script to create jupyter kernels

---
+++ setup/setup_onnx_kernels.sh
@@ -0,0 +1,9 @@
+
+conda activate onnx_training
+python -m ipykernel install --user --name onnx_training
+conda deactivate
+
+conda activate onnx_scoring
+python -m ipykernel install --user --name onnx_scoring
+conda deactivate
+
