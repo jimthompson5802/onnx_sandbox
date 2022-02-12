320a77ad374ffcf65ee8ee326c115077c9c0e340
Jim Thompson <jimthompson5802@gmail.com>
2022-02-12 13:20:58 -0500

    working example demonstrating how to wrap pretrained model in a pipeline that shares column selector transformer

--- testbed/sklearn_custom_class_pipeline_test.py
+++ testbed/sklearn_custom_class_pipeline_test.py
@@ -6,6 +6,9 @@ import numpy as np
 from sklearn.datasets import make_regression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.base import BaseEstimator
 
 N_SAMPLES = 1000
 N_FEATURES = 10
@@ -15,8 +18,21 @@ class MyRegressor(RandomForestRegressor):
         super().__init__(**kwargs)
 
     def predict_neg(self, X):
-        preds = super().predict(X)
-        return np.concatenate([preds.reshape(-1, 1), -preds.reshape(-1, 1)], axis=1)
+        y_hat = super().predict(X)
+        return np.concatenate([y_hat.reshape(-1, 1), -y_hat.reshape(-1, 1)], axis=1)
+
+class ScoringOnlyRegressor(BaseEstimator):
+    def __init__(self, trained_model):
+        super().__init__()
+        self.trained_model = trained_model
+
+    # placeholder required by sklearn API spec
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        y_hat = self.trained_model.predict_neg(X)
+        return y_hat
 
 
 def main():
@@ -30,14 +46,37 @@ def main():
     train_df, test_df = train_test_split(df, test_size=0.2)
     print(train_df.shape, test_df.shape)
 
+    # setup pipeline for training
+    # setup column selector
+    columns_to_use = ['X00', 'X09', 'X03']
+    col_selector = ColumnTransformer(
+        [('selector', 'passthrough', columns_to_use)]
+    )
+    col_selector.fit_transform(train_df)
+
     # train model
     rf = MyRegressor(random_state=123)
-    rf.fit(df.drop(['y'], axis=1), df['y'])
-    print(rf)
+    t_pipe = Pipeline(steps=[
+        ('selector', col_selector),
+        ('estimator', rf)
+    ])
 
-    print(rf.predict_neg(test_df.drop(['y'], axis=1))[:5])
+    t_pipe.fit(df.drop(['y'], axis=1), df['y'])
+    print(t_pipe)
 
-    print(rf.predict(test_df.drop(['y'], axis=1)).reshape(-1, 1)[:5])
+    print(rf.predict_neg(test_df[columns_to_use])[:5])
+
+    print(rf.predict(test_df[columns_to_use]).reshape(-1,1)[:5])
+
+    # setup proxy pipeline for scoring
+    rf2 = ScoringOnlyRegressor(rf)
+    s_pipe = Pipeline(
+        steps = [
+            ('selector', col_selector),
+            ('estimator', rf2)
+        ]
+    )
+    print(s_pipe.predict(test_df)[:5])
 
 
 if __name__ == '__main__':
