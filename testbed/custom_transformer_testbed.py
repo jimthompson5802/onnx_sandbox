ce345090a4cddfabd06c91db7d7a95c2c632c7c1
Jim Thompson <jimthompson5802@gmail.com>
2022-03-06 17:15:15 -0500

    WIP: document code construct

--- testbed/custom_transformer_testbed.py
+++ testbed/custom_transformer_testbed.py
@@ -45,33 +45,53 @@ class DFNearbyImputer(BaseEstimator, TransformerMixin):
             'constant': self._fill_in_constant
         }
 
-    def _fill_in_constant(self, X):
-        # drop the groupby column
-        column_names = X.drop([self.groupby], axis=1).columns
-        values = np.ones([len(column_names)]) * self.fill_value
-        return pd.Series({k:v for k, v in zip(column_names, values)})
+    def _fill_in_constant(self, X, **kwargs):
+        if isinstance(X, pd.DataFrame):
+            # drop the groupby column
+            # return series of fill value for each column
+            column_names = X.drop([self.groupby], axis=1).columns
+            values = np.ones([len(column_names)]) * self.fill_value
+            return pd.Series({k:v for k, v in zip(column_names, values)})
+        elif isinstance(X, pd.Series):
+            return self.fill_value
+        else:
+            raise ValueError(
+                f"Expected pandas DataFrame or Series, instead found {type(X)}"
+            )
 
     def fit(self, X:pd.DataFrame, y=None) -> object:
+        # collect column names except for groupby column
         self.feature_names_in_ = X.drop([self.groupby], axis=1).columns.to_list()
+
+        # compute missing value imputation for each groupby value for all columns
         self.nearby_imputed_values_ = X.groupby([self.groupby]).apply(self.summary_stats[self.strategy],
                                                                numeric_only=True)
+
+        # compute default missing value imputation for the case that groupby value was not seen
+        # during fit() method
         self.overall_imputed_values_ = X.drop([self.groupby], axis=1).apply(self.summary_stats[self.strategy])
-        print(f"nearby:\n{self.nearby_imputed_values_}")
-        print(f"overall:\n{self.overall_imputed_values_}")
+
         return self
 
     def transform(self, X:pd.DataFrame) -> pd.DataFrame:
+        # create shallow copy to avoid altering original object
         df = X.copy()
         for col in df.drop([self.groupby], axis=1).columns:
             for group in X[self.groupby].unique():
-                mask = (X[col].notna()) | (X[self.groupby] != group)
+                # for each column and for each groupby value find missing
+                # value locations to fill
+                mask = (X[col].isna()) & (X[self.groupby] == group)
                 try:
                     # get summary statistic for "nearby" observations
-                    df.loc[:, col] = df.loc[:, col].where(mask, other=self.nearby_imputed_values_.loc[group, col])
+                    # where() method requires logical not of mask values
+                    df.loc[:, col] = df.loc[:, col].where(np.logical_not(mask), other=self.nearby_imputed_values_.loc[group, col])
                 except KeyError:
-                    # if no summary statistic exists for the "neraby" location, use overall summary stats
-                    df.loc[:, col] = df.loc[:, col].where(mask, other=self.overall_imputed_values_.loc[col])
+                    # if no summary statistic exists for the "neraby" location, use overall default stats
+                    # where() method requires logical not of mask values
+                    df.loc[:, col] = df.loc[:, col].where(np.logical_not(mask), other=self.overall_imputed_values_.loc[col])
+
         if self.add_indicator:
+            # add indicator variables if requested
             missing_indicator = X.drop([self.groupby], axis=1).isna()
             df = pd.DataFrame(
                 np.concatenate([df, np.int8(missing_indicator)], axis=1),
@@ -102,9 +122,11 @@ def main():
     print(df.dtypes)
     print(df)
 
-    pipe = make_pipeline(DFNearbyImputer(strategy='median', groupby='ctract', add_indicator=True))
+    pipe = make_pipeline(DFNearbyImputer(strategy='median', fill_value=0,  groupby='ctract', add_indicator=True))
     print(pipe.fit_transform(df))
     print(pipe.transform(df).dtypes)
+    print(f"NEARBY:\n{pipe['dfnearbyimputer'].nearby_imputed_values_}")
+    print(f"OVERALL:\n{pipe['dfnearbyimputer'].overall_imputed_values_}")
 
     # add unknown ctract to data
     df_test = pd.concat(
